30375923e120295e85162ced4a2bf84b10162214
mesa.cristian@gmail.com <mesa.cristian@gmail.com>
2023-03-28 22:24:38 -0500

    add user tables

--- Sales.API/Data/SeedDb.cs
+++ Sales.API/Data/SeedDb.cs
@@ -2,6 +2,8 @@
 using Sales.API.Services;
 using Microsoft.EntityFrameworkCore;
 using Sales.Shared.Responses;
+using Sales.API.Helpers;
+using Sales.Shared.Enums;
 
 namespace Sales.API.Data
 {
@@ -9,10 +11,12 @@ namespace Sales.API.Data
     {
         private readonly DataContext _context;
         private readonly IApiService _apiService;
-        public SeedDb(DataContext context, IApiService apiService)
+        private readonly IUserHelper _userHelper;
+        public SeedDb(DataContext context, IApiService apiService, IUserHelper userHelper)
         {
             _context = context;
             _apiService = apiService;
+            _userHelper = userHelper;
         }
 
         public async Task SeedAsync()
@@ -20,6 +24,40 @@ namespace Sales.API.Data
             await _context.Database.EnsureCreatedAsync();
             await CheckCountriesAsync();
             await CheckCategoriesAsync();
+            await CheckRolesAsync();
+
+            await CheckUserAsync("1010", "Usuario", "Inicial", "cmesa@yopmail.com", "321 123 1234", "Calle Luna Calle Sol", UserType.Admin);
+        }
+
+        private async Task<User> CheckUserAsync(string document, string firstName, string lastName, string email, string phone, string address, UserType userType)
+        {
+            var user = await _userHelper.GetUserAsync(email);
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = phone,
+                    Address = address,
+                    Document = document,
+                    City = _context.Cities.FirstOrDefault(),
+                    UserType = userType,
+                };
+
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, userType.ToString());
+            }
+
+            return user;
+        }
+
+        private async Task CheckRolesAsync()
+        {
+            await _userHelper.CheckRoleAsync(UserType.Admin.ToString());
+            await _userHelper.CheckRoleAsync(UserType.User.ToString());
         }
 
         private async Task CheckCountriesAsync()
--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -13,6 +13,8 @@ namespace Sales.Shared.Entities
 
         public int StateId { get; set; }
         public State? State { get; set; }
-        
+
+        public ICollection<User>? Users { get; set; }
+
     }
 }
