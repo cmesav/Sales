aeeb1c2511f3651c79bdeda4a4c61fc6173a26b3
mesa.cristian@gmail.com <mesa.cristian@gmail.com>
2023-03-29 23:45:31 -0500

    add photo to register

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IFileStorage, FileStorage>();
 builder.Services.AddControllers()
     .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
