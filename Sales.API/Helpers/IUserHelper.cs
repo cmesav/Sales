11849e354d2e2724919df67d5841cba1e39010e8
mesa.cristian@gmail.com <mesa.cristian@gmail.com>
2023-03-30 20:38:36 -0500

    edit user

--- Sales.API/Helpers/IUserHelper.cs
+++ Sales.API/Helpers/IUserHelper.cs
@@ -8,6 +8,12 @@ namespace Sales.API.Helpers
     {
         Task<User> GetUserAsync(string email);
 
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<User> GetUserAsync(Guid userId);
+
         Task<IdentityResult> AddUserAsync(User user, string password);
 
         Task CheckRoleAsync(string roleName);
