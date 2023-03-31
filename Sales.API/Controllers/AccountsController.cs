d8c5692bb307cd72c34241d4de9bdae499aec07b
mesa.cristian@gmail.com <mesa.cristian@gmail.com>
2023-03-30 22:34:20 -0500

    add resend confirmation email

--- Sales.API/Controllers/AccountsController.cs
+++ Sales.API/Controllers/AccountsController.cs
@@ -57,7 +57,6 @@ namespace Sales.API.Controllers
             return NoContent();
         }
 
-
         [HttpPost("CreateUser")]
         public async Task<ActionResult> CreateUser([FromBody] UserDTO model)
         {
@@ -97,6 +96,35 @@ namespace Sales.API.Controllers
             return BadRequest(result.Errors.FirstOrDefault());
         }
 
+        [HttpPost("ResedToken")]
+        public async Task<ActionResult> ResedToken([FromBody] EmailDTO model)
+        {
+            User user = await _userHelper.GetUserAsync(model.Email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+            var tokenLink = Url.Action("ConfirmEmail", "accounts", new
+            {
+                userid = user.Id,
+                token = myToken
+            }, HttpContext.Request.Scheme, _configuration["UrlWEB"]);
+
+            var response = _mailHelper.SendMail(user.FullName, user.Email!,
+                $"Saless- Confirmación de cuenta",
+                $"<h1>Sales - Confirmación de cuenta</h1>" +
+                $"<p>Para habilitar el usuario, por favor hacer clic 'Confirmar Email':</p>" +
+                $"<b><a href ={tokenLink}>Confirmar Email</a></b>");
+
+            if (response.IsSuccess)
+            {
+                return NoContent();
+            }
+
+            return BadRequest(response.Message);
+        }
 
         [HttpPost("Login")]
         public async Task<ActionResult> Login([FromBody] LoginDTO model)
