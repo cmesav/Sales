d4d1a9ba53d2d7491be01d2240f95ae5863878b9
mesa.cristian@gmail.com <mesa.cristian@gmail.com>
2023-03-30 23:23:15 -0500

    add password recover

--- Sales.API/Helpers/IUserHelper.cs
+++ Sales.API/Helpers/IUserHelper.cs
@@ -27,6 +27,10 @@ namespace Sales.API.Helpers
 
         Task<IdentityResult> ConfirmEmailAsync(User user, string token);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
 
         Task LogoutAsync();
 
